00b627f1db88f7a07c5f243322104f40546a6ff4
DilshodXD <dilshodxdxd@gmail.com>
2023-11-27 21:34:15 +0500

    fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import HomeNavbar from "./components/Home/components/homeNavbar";
 import HomePage from "./components/Home/HomePage";
 import UserPosts from "./components/User/components/userPosts";
 import UserAbout from "./components/User/components/userAbout";
-import UserSaved from './components/User/components/userSaved'
+import UserSaved from "./components/User/components/userSaved";
 import UserSettings from "./components/User/Settings";
 
 function App() {
@@ -23,7 +23,6 @@ function App() {
           </Route>
 
           <Route path="/user" element={<UserNavbar />}>
-
             <Route path={""} element={<User />}>
               <Route index element={<UserPosts />} />
               <Route path="about" element={<UserAbout />} />
@@ -32,13 +31,6 @@ function App() {
                 path="answers"
                 element={<p className="tx-white">Javoblar</p>}
               />
-
-            <Route path={''} element={<User />}>
-              <Route index element={<UserPosts/>} />
-              <Route path="about" element={<UserAbout/>} />
-              <Route path="saved" element={<p className="tx-white">Saved</p>} /> 
-              <Route path="answers" element={<p className="tx-white">Javoblar</p>} /> 
-
             </Route>
             <Route path="settings" element={<UserSettings />} />
           </Route>
