5d71efa870901d16e55279f28c8b0bc12c05b621
DilshodXD <dilshodxdxd@gmail.com>
2023-12-06 19:21:19 +0500

    add likes for posts

--- src/components/Home/components/article.jsx
+++ src/components/Home/components/article.jsx
@@ -15,24 +15,31 @@ const people = [
       "https://miro.medium.com/v2/resize:fit:640/format:webp/0*kGal_TgMZnDg3LCf.png",
     text: "Websites are now an essential tool for every business. And if you are a web developer and need to find new material or resources, then this is the place for you. As a developer, it is not easy to find a website that can provide all the resources and information that you need. This article will take you through 12 websites that you’ll love as a developer.",
     title: "12 Websites You’ll Love As A Developer",
-    userImg:
-      "https://avatars.githubusercontent.com/u/101493630?s=400&u=c933f26b960ac32c98a88c147b27ea77accb39a0&v=4",
-    userName: "Dilshod Xojimuhammedov",
+    likes: 53,
   },
   {
-    userImg:
-      "https://avatars.githubusercontent.com/u/101493630?s=400&u=c933f26b960ac32c98a88c147b27ea77accb39a0&v=4",
-    userName: "Dilshod Xojimuhammedov",
+    userImg: "https://avatars.githubusercontent.com/u/12645328?v=4",
+    userName: "Marc Lou",
     id: "sd5a3",
-    user: "Dilshod",
+    user: "Mark",
     isPublic: false,
     postImg:
       "https://miro.medium.com/v2/resize:fit:720/format:webp/1*mCZjOe-l6BfariFv356WjA.png",
     text: "While useStateis a simple tool to use, many developers still make mistakes with it. During code reviews, I often see these mistakes made even by experienced people. I often see these mistakes made even by experienced people.",
     title: "5 React useState Mistakes That Will Get You Fired",
-    userImg:
-      "https://avatars.githubusercontent.com/u/101493630?s=400&u=c933f26b960ac32c98a88c147b27ea77accb39a0&v=4",
-    userName: "Dilshod Xojimuhammedov",
+    likes: 12,
+  },
+  {
+    userImg: "https://miro.medium.com/v2/resize:fill:176:176/1*_wgjrVvTLyrxSSD1pBa4Ew.jpeg",
+    userName: "Harendra Kumar",
+    id: "sd3a3",
+    user: "harendra21",
+    isPublic: true,
+    postImg:
+      "https://miro.medium.com/v2/resize:fit:720/format:webp/1*nX3dW9vnOTDLGjlfRZShtQ.png",
+    text: "React.js has revolutionized front-end development by providing a powerful and efficient way to build user interfaces. However, as your React application grows in complexity, maintaining clean, readable, and maintainable code becomes crucial. In this guide, we’ll explore best practices for writing clean React.js code that not only works but is also easy to understand and maintain.",
+    title: "React Js Clean Code Guide",
+    likes: 27,
   },
 ];
 
@@ -67,6 +74,7 @@ function Article() {
                 title={post.title}
                 userImg={post.userImg}
                 userName={post.userName}
+                like={post.likes}
               />
             </li>
           ))}
--- src/components/Home/components/homeLinks.jsx
+++ src/components/Home/components/homeLinks.jsx
@@ -1,51 +1,39 @@
 import { Link } from "react-router-dom";
 import "../homeComponents.scss";
 
-function Links({ link, img, title, text }) {
-  return (
-    <Link to={link} className="link-box bor-white rad-15">
-      <img className="link-img" src={img} />
-      <div className="link-inBox">
-        <p className="link-title tx-white">{title}</p>
-        <p className="link-text tx-white">{text}</p>
-      </div>
-    </Link>
-  );
-}
+const links = [
+  {
+    path: "reading",
+    img: "https://cdn.dribbble.com/userupload/2795150/file/original-80ae8932a8ffd19542821d540ac231d2.jpg?resize=752x",
+    text: "Siz bu yerda turli hil dasturlashga oid malumotlarni o’qib o’rganishingiz bilim olishingiz mumkun.",
+    title: "O’qish",
+  },
+  {
+    path: "tube",
+    img: "https://as1.ftcdn.net/v2/jpg/03/00/38/90/1000_F_300389025_b5hgHpjDprTySl8loTqJRMipySb1rO0I.jpg",
+    text: "Siz ushbu youtube kanalda turli hil dasturlashga oid videolarni ko’rishingiz va o’rganishingiz mumkun.",
+    title: "Video darslar",
+  },
+  {
+    path: "contact",
+    img: "https://static.clubs.nfl.com/image/private/f_auto/ravens/anw0lbvzibgvzpjcgjhi",
+    text: "Bu sahifadan bizning ijtimoiy tarmoqlardagi sahifalamizni topishingiz mumkun.",
+    title: "Bog’lanish",
+  },
+];
 
 function HomeLink() {
   return (
     <section className="link d-flex jcc">
-      <Links
-        link={"/"}
-        img={
-          "https://cdn.dribbble.com/userupload/2795150/file/original-80ae8932a8ffd19542821d540ac231d2.jpg?resize=752x"
-        }
-        text={
-          "Siz bu yerda turli hil dasturlashga oid malumotlarni o’qib o’rganishingiz bilim olishingiz mumkun."
-        }
-        title={"O’qish"}
-      />
-      <Links
-        link={"/"}
-        img={
-          "https://as1.ftcdn.net/v2/jpg/03/00/38/90/1000_F_300389025_b5hgHpjDprTySl8loTqJRMipySb1rO0I.jpg"
-        }
-        text={
-          "Siz ushbu youtube kanalda turli hil dasturlashga oid videolarni ko’rishingiz va o’rganishingiz mumkun."
-        }
-        title={"Video darslar"}
-      />
-      <Links
-        link={"/"}
-        img={
-          "https://static.clubs.nfl.com/image/private/f_auto/ravens/anw0lbvzibgvzpjcgjhi"
-        }
-        text={
-          "Bu sahifadan bizning ijtimoiy tarmoqlardagi sahifalamizni topishingiz mumkun."
-        }
-        title={"Bog’lanish"}
-      />
+      {links.map((item) => (
+        <Link to={item.path} className="link-box bor-white rad-15">
+          <img className="link-img" src={item.img} />
+          <div className="link-inBox">
+            <p className="link-title tx-white">{item.title}</p>
+            <p className="link-text tx-white">{item.text}</p>
+          </div>
+        </Link>
+      ))}
     </section>
   );
 }
